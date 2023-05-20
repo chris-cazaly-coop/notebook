d612fc65d59454132e9137dd66eaa8b608983b2d
Chris Cazaly <chris.cazaly@coop.co.uk>
2023-05-20 02:45:47 +0100

    CSS and functional changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pages, setPages] = useState([{ id: 1, textContent: "" }]);
+  const [currentPageIndex, setCurrentPageIndex] = useState(0);
+
+  function handlePreviousPage() {
+    if (currentPageIndex === 0) return;
+    setCurrentPageIndex(currentPageIndex - 1);
+  }
+  function handleNextPage() {
+    if (currentPageIndex === pages.length - 1) {
+      const newPages = [...pages];
+      newPages.push({ id: pages.length + 1, textContent: "" });
+      setPages(newPages);
+      setCurrentPageIndex(pages.length);
+    } else {
+      setCurrentPageIndex(currentPageIndex + 1);
+    }
+  }
+  function handleTextInput(event) {
+    const newPages = [...pages];
+    newPages[currentPageIndex].textContent = event.target.value;
+    setPages(newPages);
+  }
+  function adjustTextAreaHeight() {
+    const textArea = document.querySelector("textarea");
+    textArea.style.height = "auto"; // Reset the height to auto to recalculate the scrollHeight
+    textArea.style.height = textArea.scrollHeight + "px";
+  }
+
+  useEffect(() => {
+    adjustTextAreaHeight(); // Adjustment of textarea height when content changes
+  }, [pages, currentPageIndex]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App</h1>
+      <div className="text-area-wrapper">
+        <textarea
+          onInput={handleTextInput}
+          value={pages[currentPageIndex].textContent}
+        />
+      </div>
+      <br />
+      <div className="button-container">
+        <button onClick={handlePreviousPage}>Previous page</button>
+        <button onClick={handleNextPage}>Next page</button>
+      </div>
     </div>
   );
 }
